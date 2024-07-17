f5e27fe63b1985194547f654c637bf26a4e882c5
Kattirsitti Jeet Govindrao <138518455+tarantulass@users.noreply.github.com>
2024-07-17 15:44:58 +0530

    Updated code for better readability

--- pytorch_ann_classifier.py
+++ pytorch_ann_classifier.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-
 ## **Mounting Google Drive**
-First, we need to import the dataset of images from your Google Drive. To do so, **run the below cell**. This will mount your Drive to the running Colab instance. Then, you will be able to access all your Google Drive data in this notebook.
-"""
 
 from google.colab import drive
 drive.mount('/content/drive')
 
-"""## **Import the Libraries**
-"""
-
+## **Import the Libraries**
 # Commented out IPython magic to ensure Python compatibility.
 import os
 import numpy as np
@@ -20,7 +13,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 # %matplotlib inline
 
-"""## **Importing the Dataset**
+"""
+Importing the Dataset
 
 We now need to import the dataset from the Google Drive. Below given is a function named `getdata`, which takes the path of the folder from which data is to be imported as its argument.
 You are required to give in the path of the folder in which the images of `pizza` and `not_pizza` are saved in your Google Drive. 
@@ -53,10 +47,9 @@ data = not_pizza_data # pizza_data (or) not_pizza_data
 
 plt.imshow(data[index].int())
 
-"""## **Preprocessing the dataset**
+"""Preprocessing the dataset
 
 Before we begin working with the neural network, we need to make sure our data (in this case, $64\times64$ RGB images) is in a format that neural networks can work with.
-
 First, we create a `train_data` collection of images for training the neural network, and another collection `test_data` that will then be used to check the accuracy of the trained neural network. We do this by taking slices of the total data and concatenating them.  
 """
 
@@ -98,7 +91,8 @@ test_labels = torch.cat((torch.ones(50,1),torch.zeros(50,1)),0)
 
 print(train_labels.shape, test_labels.shape)
 
-"""##**Building the Neural Network**
+"""
+Building the Neural Network
 
 First, initialise the hyperparameters of the neural network.  
 Here, we are going to make a 3 layer neural network (i.e., 1 input layer, 2 hidden layers and 1 output layer). The first hidden layer will have 10 nodes, while the second will have 12.   
@@ -113,10 +107,8 @@ D_out = 1
 #since it is pizza or not pizza hence we can use only one neuron for this purpose as it ensures both are independent
 
 """Now you will make the actual model. The Model includes the use of `Linear` function at each layer, alongside non-linear activation functions.
-
 We are going to use `ReLU` functions as activation functions for the input and the first hidden layer and `Sigmoid` function for the final output, as we want an output between 0 and 1.
-
-(Hint: `Linear`, `ReLU` and `Sigmoid` functions are a part of the `nn` module of the **PyTorch** library)
+(Hint: `Linear`, `ReLU` and `Sigmoid` functions are a part of the `nn` module of the PyTorch library)
 """
 
 model = torch.nn.Sequential(
@@ -136,11 +128,7 @@ model(final_train_data[0]) #To check if the model works
 """After making the model, we defined the `loss_fn` , i.e., the loss function as the Binary Cross Entropy Loss.
 
 Here, you will implement gradient descent.
-
 The `learning_rate` is the step size which the neural network takes when it updates the parameters of the network. (Try to explore various values of step size. Values similar to 0.005 are usually suitable). Also play around with the number of `iterations` the gradient descent algorithm needs to take.
-
-**Follow the steps given as per the comments.**
-
 *Note: When you run the code, it could take about 5 minutes for the network to finish training.*
 """
 
@@ -177,7 +165,7 @@ torch.save(model, 'model_best.pt')
 y_pred = model(final_train_data[0])
 y_pred.shape == train_labels[0].shape
 
-"""## **Checking the Results**
+"""Checking the Results
 Now that we have used the training set to train the network, we shall use the test set to check how the neural network performs with new inputs.
 """
 
@@ -187,7 +175,6 @@ plt.imshow(test_data[index].int())
 print (f'According to the neural network, index = {index} is {"a pizza" if model(final_test_data[index]) > 0.5 else "not a pizza"}' )
 
 """To quantize how accurately the neural network is able to classify images, we have defined a helper `predict` function that takes a dataset and returns the fraction of times the neural network correctly classified the image.  
-
 Complete the function such that it prints the correct accuracy of its predictions.
 """
 
@@ -201,7 +188,7 @@ def predict(model, data, labels):
 
     print("Accuracy: "  + str(torch.sum((predictions == labels)).item()/predictions.shape[0]))
 
-"""**Run** the cell below to find out the accuracy of your model for the training and test datasets."""
+"""Run the cell below to find out the accuracy of your model for the training and test datasets."""
 
 predict(model, final_train_data, train_labels)
 predict(model,final_test_data,test_labels)
